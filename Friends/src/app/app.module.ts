b453c5e6ec58307edd1bd4ec98b3510985b826e9
rbasehewa <rbasehewa@gmail.com>
2021-10-02 17:29:58 +1000

    added FormsModule to fit together importing frineds components

--- Friends/src/app/app.module.ts
+++ Friends/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
+import { FriendsComponent } from './friends/friends.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
