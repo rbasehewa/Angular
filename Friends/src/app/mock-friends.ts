f0b256e6781e64f8c037876aa42e5f4993027482
rbasehewa <rbasehewa@gmail.com>
2021-10-05 10:30:46 +1100

    adding mock data, except css file, when clicking user you can edit the data

--- Friends/src/app/mock-friends.ts
+++ Friends/src/app/mock-friends.ts
@@ -1,14 +1,14 @@
 import { Friend } from "./friend";
 
 export const FRIENDS: Friend[] = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Dr Chathu' },
+    { id: 12, name: 'Venura' },
+    { id: 13, name: 'Avanthi' },
+    { id: 14, name: 'Rajitha' },
+    { id: 15, name: 'Sashika' },
+    { id: 16, name: 'Manoj' },
+    { id: 17, name: 'Sameera' },
+    { id: 18, name: 'Dr Rukshan' },
+    { id: 19, name: 'Mitree' },
+    { id: 20, name: 'Yoshantha' }
   ];
