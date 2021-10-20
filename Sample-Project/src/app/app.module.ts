2af04ca303f8b36e134f12e7fad39d41160569a3
rbasehewa <rbasehewa@gmail.com>
2021-10-20 22:23:18 +1100

    pratice DI repo

---
+++ Sample-Project/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AuthorsService } from './authors.service';
+import { AuthorsComponent } from './authors/authors.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthorsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    AuthorsService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
